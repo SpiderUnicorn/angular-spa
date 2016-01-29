96f865b7d2291dcc347571e9de43472a4f5b8f58
Johan Nyström <johannystrom85@gmail.com>
2016-01-29 22:05:54 +0100

    Added task descriptions to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-/*jslint node: true */
+/*
+    Avalible tasks:
 
+    gulp inject      ->    inject js / css into index.html
+    gulp lint        ->    analyze javascript code quality
+    gulp styles      ->    autoprefix css
+    gulp test        ->    run all unit tests
+    gulp watch       ->    watch for file changes and call inject
+
+    gulp --tasks     ->    view all avalible tasks
+
+    You'll find the implementations of the tasks under
+    /gulp
+
+*/
+
+
+/*jslint node: true */
 var gulp = require('gulp');
 
 var requireDir = require('require-dir');
